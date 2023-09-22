93b725901aa20a15328b87e2a0821d1afb26abbf
MariaDobry <110995691+MariaDobry@users.noreply.github.com>
2023-09-21 23:43:57 -0300

    Add files via upload

---
+++ Alura-midi-main/Alura-midi-main/aluramidi-curso-arquivos-iniciais/main.js
@@ -0,0 +1,35 @@
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if(elemento && elemento.localName === 'audio') {
+         elemento.play();
+    }
+    else {
+        console.log('elemento não encontrado ou seletor inválido');
+    }
+}
+
+const listaDeteclas = document.querySelectorAll('.tecla');
+
+for (let contador = 0; contador < listaDeteclas.length; contador++) {
+
+    const tecla = listaDeteclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (evento) {
+
+        if (evento.code ==='Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+        
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa')
+    }
+}
